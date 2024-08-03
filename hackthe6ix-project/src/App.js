795584fd2d09a94ad8b3e99fae703a76a5adf6ba
Tolu <emoruwatoluwanimi@gmail.com>
2024-08-03 16:06:42 -0400

    Background setup for the kitchen

--- hackthe6ix-project/src/App.js
+++ hackthe6ix-project/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
       <div className="App">
+        <Navbar />
         <div className='app-body'></div>
       </div>
   );
---
+++ hackthe6ix-project/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+
+export default function Navbar(){
+
+    return(
+        <div className='nav-body'>
+            <div className='nav-logo'>
+
+            </div>
+            <div className='nav-info'>
+
+            </div>
+        </div>
+    )
+}
