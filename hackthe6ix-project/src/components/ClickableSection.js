d0a0a2b41fb8849e33ac19f0d9403ee54fb8bca0
Tolu <emoruwatoluwanimi@gmail.com>
2024-08-03 16:35:59 -0400

    Fridge inventory opening

--- hackthe6ix-project/src/components/ClickableSection.js
+++ hackthe6ix-project/src/components/ClickableSection.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import './clickable-section.css';
 
 export default function ClickableSection(){
+
+    const [openFridge, setOpenFridge] = useState(false);
+
+    const handleFridgeClick = () => {
+        console.log('fridge has been clicked');
+        setOpenFridge(!openFridge)
+    }
+
     return(
         <div className="click-body">
-            <div className="fridge-body" onClick={() => {console.log('fridge has been clicked')}}>
+            <div className="fridge-body" onClick={() => handleFridgeClick()}>
+            </div>
+            <div className={`fridge-content ${openFridge ? 'show' : ''}`}>
+                <div className={`fridge-info ${openFridge ? 'show' : ''}`}>
 
+                </div>
             </div>
         </div>
     )
