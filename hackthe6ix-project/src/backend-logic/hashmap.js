615665863f8ee4b1c9f9ed9d55b923073cc00974
justinwang <justinwang407@gmail.com>
2024-08-03 20:15:23 -0400

    Added SVG Mapping

---
+++ hackthe6ix-project/src/backend-logic/hashmap.js
@@ -0,0 +1,14 @@
+const foodMap = new Map();
+
+// Matching food to database path
+foodMap.set("Apple", "./images/sprites/apple.svg");
+foodMap.set("banana", "./images/sprites/banana.svg");
+foodMap.set("beef", "./images/sprites/beef.svg");
+foodMap.set("bread", "./images/sprites/bread.svg");
+foodMap.set("broccoli", "./images/sprites/broccoli.svg");
+foodMap.set("cheese", "./images/sprites/cheese.svg");
+foodMap.set("chicken", "./images/sprites/chicken.svg");
+foodMap.set("egg", "./images/sprites/egg.svg");
+foodMap.set("onion", "./images/sprites/onion.svg");
+foodMap.set("pork", "./images/sprites/pork.svg");
+foodMap.set("strawberry", "./images/sprites/strawberry.svg");
