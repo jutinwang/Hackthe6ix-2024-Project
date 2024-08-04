bb79c55216b78523c7aa50e25e5f460b2e4abeb8
Tolu <emoruwatoluwanimi@gmail.com>
2024-08-04 02:49:39 -0400

    Backend no errors

--- project-backend/openai-api.js
+++ project-backend/openai-api.js
@@ -1,6 +1,7 @@
 const OpenAI = require('openai');
+
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
